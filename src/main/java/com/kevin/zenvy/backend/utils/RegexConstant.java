72114ce4814264451e4309ae90c71ef1f43f35a2
kevGuido22 <guidourbinakevin@gmail.com>
2026-05-24 16:15:09 -0600

    feat: add PASSWORD_REGEX

---
+++ src/main/java/com/kevin/zenvy/backend/utils/RegexConstant.java
@@ -0,0 +1,5 @@
+package com.kevin.zenvy.backend.utils;
+
+public class RegexConstant {
+    public static final String PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&.])[A-Za-z\\d@$!%*?&.]{8,}$";
+}
