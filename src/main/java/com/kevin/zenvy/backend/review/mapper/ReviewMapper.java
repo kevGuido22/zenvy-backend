ee76a185e440ad8c61c28e2efa477dfb4b68e404
kevGuido22 <guidourbinakevin@gmail.com>
2026-06-20 15:41:02 -0600

    feat: add images field in ReviewMapper

--- src/main/java/com/kevin/zenvy/backend/review/mapper/ReviewMapper.java
+++ src/main/java/com/kevin/zenvy/backend/review/mapper/ReviewMapper.java
@@ -1,10 +1,13 @@
 package com.kevin.zenvy.backend.review.mapper;
 
+import com.kevin.zenvy.backend.image.dto.ImageResponseDTO;
 import com.kevin.zenvy.backend.place.dto.PlaceBasicDTO;
 import com.kevin.zenvy.backend.review.dto.ReviewResponseDTO;
 import com.kevin.zenvy.backend.review.model.Review;
 import com.kevin.zenvy.backend.user.dto.UserBasicDTO;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ReviewMapper {
@@ -23,12 +26,22 @@ public class ReviewMapper {
                 .category(review.getPlace().getCategory())
                 .build();
 
+        List<ImageResponseDTO> images = review.getImages()
+                .stream()
+                .map(image -> ImageResponseDTO.builder()
+                        .id(image.getId())
+                        .imageUrl(image.getImageUrl())
+                        .fileKey(image.getFileKey())
+                        .build())
+                .toList();
+
         return ReviewResponseDTO.builder()
                 .id(review.getId())
                 .rating(review.getRating())
                 .comment(review.getComment())
                 .user(user)
                 .place(place)
+                .images(images)
                 .build();
     }
 }
